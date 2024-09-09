9fff0784f0baed358b0bd78c2fafc819bbe096c3
shidang233 <79011008+shidang233@users.noreply.github.com>
2024-09-09 14:29:42 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -329,7 +329,7 @@ export default function Home() {
   return (
     <main className=" overflow-auto h-full flex w-full min-h-screen flex-col items-center justify-between">
       <header className="fixed top-0 h-[50px] left-0 w-full border-b bg-white flex z-50 justify-center items-center">
-        <nav className="flex justify-between items-center w-full max-w-4xl px-4">图床</nav>
+        <nav className="flex justify-between items-center w-full max-w-4xl px-4">云霜的图床</nav>
 
         <Link href="https://img.ypshidifu.cn"><button className="px-4 py-2 mx-2 w-28  sm:w-28 md:w-20 lg:w-16 xl:w-16  2xl:w-20 bg-blue-500 text-white rounded ">友链图床</button></Link>
       </header>
